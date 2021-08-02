e4dc7163e9287e1cd0a06c781403de3e5b4e9806
medeirosinacio <medeirosinacio@outlook.com>
2021-08-02 19:04:38 -0300

    add animation home

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,32 @@
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let typeText = document.querySelector(".typeText")
+let textToBeTypedArr = ["e Analista", "e Devops", "e Curioso", "", "", "", "", ""]
+let index = 0, isAdding = true, textToBeTypedIndex = 0
+
+function playAnim() {
+    setTimeout(function () {
+        typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
+        if (isAdding) {
+            if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+                isAdding = false
+                setTimeout(function () {
+                    playAnim()
+                }, 2000)
+                return
+            } else {
+                index++
+            }
+        } else {
+            if (index === 0) {
+                isAdding = true
+                textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
+            } else {
+                index--
+            }
+        }
+        playAnim()
+    }, isAdding ? 110 : 60)
+}
+
+setTimeout(function () {
+    playAnim()
+}, 8000);
