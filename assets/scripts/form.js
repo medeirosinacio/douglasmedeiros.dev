22659b09c6196acb8b323d7c495f0b5ba286cc41
medeirosinacio <medeirosinacio@outlook.com>
2021-08-30 23:15:07 -0300

    add contact form

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -56,25 +56,25 @@ function validValues() {
     }
 
     if (!validateName(document.getElementById('name').value)) {
-        const errorElement = document.getElementById('submitErrorBox');
+        const errorElement = document.getElementById('submitErrorBoxMessage');
         errorElement.innerHTML = 'O nome deve ter entre 5 e 50 caracteres.';
         return false;
     }
 
     if (!validatePhone(document.getElementById('phone').value)) {
-        const errorElement = document.getElementById('submitErrorBox');
-        errorElement.innerHTML = 'Telefone inválido.';
+        const errorElement = document.getElementById('submitErrorBoxMessage');
+        errorElement.innerHTML = 'O Telefone informado é inválido.';
         return false;
     }
 
     if (!validateEmail(document.getElementById('email').value)) {
-        const errorElement = document.getElementById('submitErrorBox');
-        errorElement.innerHTML = 'Email inválido.';
+        const errorElement = document.getElementById('submitErrorBoxMessage');
+        errorElement.innerHTML = 'O Email informado é inválido.';
         return false;
     }
 
     if (!validateMessage(document.getElementById('message').value)) {
-        const errorElement = document.getElementById('submitErrorBox');
+        const errorElement = document.getElementById('submitErrorBoxMessage');
         errorElement.innerHTML = 'A mensagem deve ter entre 10 e 250 caracteres.';
         return false;
     }
@@ -84,9 +84,31 @@ function validValues() {
 
 async function handleSubmit(form) {
 
+    function success() {
+        document.getElementById("staticform").classList.add('d-none');
+        document.getElementById("success-message").classList.remove('d-none');
+        document.getElementById("successAnimation").classList.add('animated');
+        document.getElementById("submitFormBtn").removeAttribute("disabled");
+        document.getElementById("name").value = '';
+        document.getElementById("phone").value = '';
+        document.getElementById("email").value = '';
+        document.getElementById("message").value = '';
+        document.getElementById("staticform").setAttribute('data-send', 'true');
+    }
+
     const errorElement = document.getElementById('submitErrorBox');
     errorElement.classList.add("d-none");
     document.getElementById("submitFormBtn").setAttribute("disabled", 'true');
+
+    if (document.getElementById("staticform").getAttribute('data-send') === "true") {
+        document.getElementById('success-message-box').innerHTML =
+            "<span class='font-Lato-Bold fs-3'>Mensagem já enviada!</span><br>\n" +
+            "Calma esse coração <3, é só aguardar que entro em contato em breve.";
+        setTimeout(function () {
+            success();
+        }, 2000);
+        return false;
+    }
 
     const formDataObj = Object.fromEntries(new FormData(form).entries());
 
@@ -108,20 +130,25 @@ async function handleSubmit(form) {
         if (json.success) {
 
             setTimeout(function () {
-                document.getElementById("modal").click();
-                document.getElementById("submitFormBtn").removeAttribute("disabled");
+                success();
             }, 3000);
 
         } else {
-            const errorElement = document.getElementById('submitErrorMessage');
+            const errorElement = document.getElementById('submitErrorBoxMessage');
             errorElement.classList.remove("d-none");
             errorElement.innerHTML = 'Ocorreu um erro no envio do formulário, atualize a página para tentar novamente ou volte mais tarde.';
             document.getElementById("submitFormBtn").removeAttribute("disabled");
         }
     } catch (e) {
-        const errorElement = document.getElementById('submitErrorMessage');
+        const errorElement = document.getElementById('submitErrorBoxMessage');
         errorElement.classList.remove("d-none");
         errorElement.innerHTML = 'Ocorreu um erro no envio do formulário, atualize a página para tentar novamente ou volte mais tarde.';
         document.getElementById("submitFormBtn").removeAttribute("disabled");
     }
+}
+
+function showForm() {
+    document.getElementById("staticform").classList.remove('d-none');
+    document.getElementById("success-message").classList.add('d-none');
+    document.getElementById("successAnimation").classList.remove('animated');
 }
