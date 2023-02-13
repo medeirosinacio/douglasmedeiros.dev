6c1e6e63634a4721f231fe9a162b9cc3d5313d0c
Douglas Medeiros <eu@douglasmedeiros.dev>
2023-02-13 15:18:27 -0300

    feat: update profile experience 2023

--- server.sh
+++ server.sh
@@ -6,5 +6,5 @@ docker run -ti --rm \
   -p 80:4000 \
   -p 4000:4000 \
   -p 35729:35729 \
-  jekyll/builder \
+  jekyll/builder:3.8 \
   /bin/bash -c "bundle install && bundle exec jekyll serve --host 0.0.0.0 --livereload"
