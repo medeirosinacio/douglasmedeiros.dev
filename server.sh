c63e11623186b876ecd786b89593cff9abc43142
Douglas Medeiros <douglas.medeiros@picpay.com>
2024-02-05 13:10:00 -0300

    refactor: adjust server.sh script

--- server.sh
+++ server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker run -ti --rm \
-  -v $PWD:/app \
+  -v ./:/app \
   -w /app \
   --dns 8.8.8.8 \
   -p 80:4000 \
   -p 4000:4000 \
   -p 35729:35729 \
   jekyll/builder:3.8 \
-  /bin/bash -c "bundle install && bundle exec jekyll serve --host 0.0.0.0 --livereload"
+  /bin/bash -c "bundle install --path=./.gem-cache && bundle exec jekyll serve --host 0.0.0.0 --livereload"
